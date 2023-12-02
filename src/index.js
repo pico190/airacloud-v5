7f7ab128a05450b8fe97b5a0863da3d653fedffc
pico190 <saulbuesosanz@gmail.com>
2023-12-02 22:21:27 +0100

    clear all

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { sendToVercelAnalytics } from './vitals';
 
 ReactDOM.render(
   <React.StrictMode>
