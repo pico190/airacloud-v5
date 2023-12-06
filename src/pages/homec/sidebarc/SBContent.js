4da7c0b8ca53599c47f88086d78de03fe031ab3d
pico190 <saulbuesosanz@gmail.com>
2023-12-06 20:49:11 +0100

    i want to eat potatoes

--- src/pages/homec/sidebarc/SBContent.js
+++ src/pages/homec/sidebarc/SBContent.js
@@ -1,8 +1,8 @@
 export function SBContent() {
-    return (<div classList="SBContent">
-        <div classList="aira_logo">
+    return (<div className="SBContent">
+        <div className="aira_logo">
             <img width="54px" src="https://airacloud-v5.vercel.app/icon.svg" loading="lazy" alt=""/>
-            <div classList="aira_logo_title">
+            <div className="aira_logo_title">
                 <h1>AiraCloud</h1>
                 <span>BETA</span>
             </div>
