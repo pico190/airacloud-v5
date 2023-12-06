2710d20326d7b8ac91070bfd9c8f5fbe89dbefe5
pico190 <saulbuesosanz@gmail.com>
2023-12-06 20:45:18 +0100

    +sidebarcontent

--- src/pages/homec/Sidebar.js
+++ src/pages/homec/Sidebar.js
@@ -1,8 +1,12 @@
+import { SBContent } from './sidebarc/SBContent'
+
 export function SideBar() {
     return (
         <>
         <div class="sidebar">
-            jamon sexual
+            
+          <SBContent />  
+
         </div>
         <img loading="lazy" src="https://airacloud-v5.vercel.app/aira.sidebar.svg" className="colors-img" alt=""/>
         </>
---
+++ src/pages/homec/sidebarc/SBContent.js
@@ -0,0 +1,11 @@
+export function SBContent() {
+    <div classList="SBContent">
+        <div classList="aira_logo">
+            <img width="54px" src="https://airacloud-v5.vercel.app/icon.svg" loading="lazy" alt=""/>
+            <div classList="aira_logo_title">
+                <h1>AiraCloud</h1>
+                <span>BETA</span>
+            </div>
+        </div>
+    </div>
+}
