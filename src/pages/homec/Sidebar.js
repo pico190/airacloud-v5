43d7d5b24a28f88d74865a74f59b065a79b93da7
pico190 <saulbuesosanz@gmail.com>
2023-12-06 20:25:37 +0100

    +optixfeaturepatch

--- src/pages/homec/Sidebar.js
+++ src/pages/homec/Sidebar.js
@@ -1,7 +1,10 @@
 export function SideBar() {
     return (
+        <>
         <div class="sidebar">
             jamon sexual
         </div>
+        <img loading="lazy" src="https://airacloud-v5.vercel.app/aira.sidebar.svg" alt=""/>
+        </>
     )
 }
