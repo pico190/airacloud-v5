873b65439ee62331b01a9fdaf70189d92f471e7c
pico190 <saulbuesosanz@gmail.com>
2023-12-06 21:31:00 +0100

    +span

--- src/pages/homec/sidebarc/SBContent.js
+++ src/pages/homec/sidebarc/SBContent.js
@@ -10,10 +10,12 @@ export function SBContent() {
         <div className="sb_separator"></div>
         <div className="aira_info">
                 <img width="54px" src="https://airacloud-v5.vercel.app/icon.svg" loading="lazy" alt=""/>
-                <font style={{fontSize: "26px", opacity: "0.75"}}><b>AiraCloud</b> Release 1.0</font>
-                <br /><font style={{fontSize: "12px", opacity: "0.25"}}>Engine v5.1.69</font>
-                <br /><font style={{fontSize: "12px", opacity: "0.25"}}>Last update 32 minues ago. <u>See changelog</u>.</font>
-                <br /><font style={{fontSize: "12px", opacity: "0.5"}}>(c) 2023 Aira / Made with 💚 by Swiftly</font>
+                <span>
+                    <font style={{fontSize: "26px", opacity: "0.75"}}><b>AiraCloud</b> Release 1.0</font>
+                    <br /><font style={{fontSize: "12px", opacity: "0.25"}}>Engine v5.1.69</font>
+                    <br /><font style={{fontSize: "12px", opacity: "0.25"}}>Last update 32 minues ago. <u>See changelog</u>.</font>
+                    <br /><font style={{fontSize: "12px", opacity: "0.5"}}>(c) 2023 Aira / Made with 💚 by Swiftly</font>
+                </span>
         </div>
     </div>)
 }
