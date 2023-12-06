e4a41dc4631dd2cc9e4823d9ed236d0323f5bf2f
pico190 <saulbuesosanz@gmail.com>
2023-12-06 21:19:46 +0100

    addtempinfo

--- src/pages/homec/sidebarc/SBContent.js
+++ src/pages/homec/sidebarc/SBContent.js
@@ -7,5 +7,15 @@ export function SBContent() {
                 <span>BETA</span>
             </div>
         </div>
+        <div className="sb_separator"></div>
+        <div className="aira_info">
+            <div className="aira_info_title">
+                <img width="54px" src="https://airacloud-v5.vercel.app/icon.svg" loading="lazy" alt=""/>
+                <h1 style={{fontWeight: "normal"}}><b>AiraCloud</b> Release 1.0</h1>
+            </div>
+            <font>Engine v5.1.69</font>
+            <font>Last update 32 minues ago. <u>See changelog</u>.</font>
+            <font>(c) 2023 Aira / Made with 💚 by Swiftly</font>
+        </div>
     </div>)
 }
