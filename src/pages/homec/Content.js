512e7fd3504eaebfcfb9bb040b1cb4e5a6627aae
pico190 <saulbuesosanz@gmail.com>
2023-12-07 12:34:07 +0100

    +iconfix

--- src/pages/homec/Content.js
+++ src/pages/homec/Content.js
@@ -1,34 +1,14 @@
 
+import { Button } from './contentc/Buttons'
+
 function Recent() {
-
-    function Button({ icon, icontype, name }) {
-        if(icontype === "airaicon") {
-            var iconurl = `https://airacloud-v5.vercel.app/airaicons/${icon}.svg`;
-        } else {
-            var iconurl = `https://xploit.men/scdn/?fluenticons&amp;name=${icon}&amp;type=fluent&amp;fill=white`;
-        }
-        
-        return (
-            <div className="default-content-button">
-                <img src={iconurl} loading="lazy" alt="" />
-                <b className="content-button-text">
-                    {name}
-                </b>
-                <div className="spacer" />
-                <img src="https://xploit.men/scdn/?fluenticons&amp;name=chevron-right&amp;type=fluent&amp;fill=white" loading="lazy" alt=""/>
-            </div>
-        )
-    }
-
-
-
     return (
         <div style={{marginTop: "15px"}}>
                 <font style={{fontSize: "30px", textDecoration: "underline"}}>Recent activity</font>
                 <div className="recent-activity">
                     <Button icon="astro" icontype="airaicon" name="AiraCloud en Astro" />
-                    <Button icon="astro" icontype="airaicon" name="Nature Porn" />
-                    <Button icon="astro" icontype="airaicon" name="Swiftly Team" />
+                    <Button icon="react" icontype="airaicon" name="Nature Porn" />
+                    <Button icon="people_team" icontype="fluent" name="Swiftly Team" />
                 </div>
         </div>
     )
---
+++ src/pages/homec/contentc/Buttons.js
@@ -0,0 +1,19 @@
+
+export function Button({ icon, icontype, name }) {
+    if(icontype === "airaicon") {
+        var iconurl = `https://airacloud-v5.vercel.app/airaicons/${icon}.svg`;
+    } else {
+        var iconurl = `https://xploit.men/scdn/?fluenticons&amp;name=${icon}&amp;type=fluent&amp;fill=white`;
+    }
+    
+    return (
+        <div className="default-content-button">
+            <img src={iconurl} loading="lazy" alt="" width="32" />
+            <b className="content-button-text">
+                {name}
+            </b>
+            <div className="spacer" />
+            <img src="https://xploit.men/scdn/?fluenticons&amp;name=chevron-right&amp;type=fluent&amp;fill=white" loading="lazy" alt=""/>
+        </div>
+    )
+}
