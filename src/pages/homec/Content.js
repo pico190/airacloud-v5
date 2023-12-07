3b05a6853360e22a1286ee96a3b7035100aa7385
pico190 <saulbuesosanz@gmail.com>
2023-12-07 12:30:22 +0100

    +buttonicons

--- src/pages/homec/Content.js
+++ src/pages/homec/Content.js
@@ -1,9 +1,16 @@
 
 function Recent() {
 
-    function Button({ name }) {
+    function Button({ icon, icontype, name }) {
+        if(icontype === "airaicon") {
+            var iconurl = `https://airacloud-v5.vercel.app/airaicons/${icon}.svg`;
+        } else {
+            var iconurl = `https://xploit.men/scdn/?fluenticons&amp;name=${icon}&amp;type=fluent&amp;fill=white`;
+        }
+        
         return (
             <div className="default-content-button">
+                <img src={iconurl} loading="lazy" alt="" />
                 <b className="content-button-text">
                     {name}
                 </b>
@@ -19,9 +26,9 @@ function Recent() {
         <div style={{marginTop: "15px"}}>
                 <font style={{fontSize: "30px", textDecoration: "underline"}}>Recent activity</font>
                 <div className="recent-activity">
-                    <Button name="AiraCloud en Astro" />
-                    <Button name="Nature Porn" />
-                    <Button name="Swiftly Team" />
+                    <Button icon="astro" icontype="airaicon" name="AiraCloud en Astro" />
+                    <Button icon="astro" icontype="airaicon" name="Nature Porn" />
+                    <Button icon="astro" icontype="airaicon" name="Swiftly Team" />
                 </div>
         </div>
     )
