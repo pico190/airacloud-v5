55fc2bc868ec6c713ae73d427fb252a60f3da5f6
pico190 <saulbuesosanz@gmail.com>
2023-12-15 18:59:37 +0100

    -tempErrorRedirect

--- src/utils/SwiftlyIDManager.js
+++ src/utils/SwiftlyIDManager.js
@@ -1,79 +1,64 @@
 import $ from 'jquery';
+import { getCookieValue } from './GetCookieValue';
 
 export function StartSwiftlyIDClient() {
 
     function loadByToken(token) {
-        return new Promise((resolve, reject) => {
-            $.get("https://xploit.men/swiftly/api/swiftlyid/getinfo/getname.php?token=" + token, (data) => {
-                if (data === "Error 12xGcidT9pwcVnsx3MXWVw$3R&j7W6") {
-                    reject("Error: Invalid token");
-                } else {
-                    const username = data;
-                    const response = {
-                        'token': token,
-                        'username': username
-                    };
-                    resolve(response);
+
+        var response = {};
+        console.log(token)
+        $.get("https://xploit.men/swiftly/api/swiftlyid/getinfo/getname.php?token="+token, (data) => {
+            if(data==="Error 12xGcidT9pwcVnsx3MXWVw$3R&j7W6") {
+                // window.location.replace("https://xploit.men/aira/swiftlyid-error.php");
+            } else {
+                username = data;
+                response = {
+                    'token': token,
+                    'username': username
                 }
-            });
+            }
         });
+
+        return response;
     }
 
     function loadByRevokeCode(revokeCode) {
-        return new Promise((resolve, reject) => {
-            document.cookie = "revokeCode=" + revokeCode + "; SameSite=Strict; Secure;";
-            let token = "";
-            $.get("https://xploit.men/swiftly/api/swiftlyid/gettoken?revokeCode=" + revokeCode, (data) => {
-                if (data === "Error 12xGcidT9pwcVnsx3MXWVw$3R&j7W6") {
-                    reject("Error: Invalid revoke code");
-                } else {
-                    token = data;
-                    loadByToken(token)
-                        .then((response) => {
-                            resolve(response);
-                        })
-                        .catch((error) => {
-                            reject(error);
-                        });
-                }
-            });
-        });
+        
+        // var username = "";
+        // var token = "";
+        // var response__ = "";
+        // $.get("https://xploit.men/swiftly/api/swiftlyid/gettoken?revokeCode="+revokeCode, (data) => {
+        //     if(data==="Error 12xGcidT9pwcVnsx3MXWVw$3R&j7W6") {
+        //         window.location.replace("https://xploit.men/aira/swiftlyid-error.php");
+        //     } else {
+        //         token = data;
+        //         response__ = loadByToken(token);
+        //     }
+        // });
+        console.log(revokeCode)
     }
 
-    if (!window.location.href.includes("?revokeCode=") && !window.location.href.includes("!")) {
-        window.location.replace("https://xploit.men/swiftly/login/aira?redirect_uri=" + window.location.href);
+    if(!window.location.href.includes("?revokeCode=") && !window.location.href.includes("!/")) {
+        window.location.replace("https://xploit.men/swiftly/login/aira?redirect_uri="+window.location.href)
         return false;
     } else {
-        let response__ = "";
-        if (window.location.href.includes("?revokeCode=")) {
-            const revokeCode = window.location.href.split("?revokeCode=")[1];
+        if(window.location.href.includes("?revokeCode=")) {
+            var revokeCode = window.location.href.split("?revokeCode=")[1];
             loadByRevokeCode(revokeCode)
-                .then((response) => {
-                    response__ = response;
-                    if (!window.location.href.includes("!")) {
-                        window.location.replace("/!/");
-                    }
-                })
-                .catch((error) => {
-                    window.location.replace("https://xploit.men/aira/swiftlyid-error.php");
-                });
         } else {
-            const revokeCode = getCookieValue("revokeCode");
-            if (revokeCode !== "" && revokeCode !== undefined && revokeCode !== null) {
-                window.location.replace("https://xploit.men/swiftly/login/aira?redirect_uri=" + window.location.href);
-            } else {
+            if(getCookieValue("revokeCode")!=="" && getCookieValue("revokeCode")!==undefined && getCookieValue("revokeCode")!==null) {
+                window.location.replace("https://xploit.men/swiftly/login/aira?redirect_uri="+window.location.href)
+            }  else {
+                var revokeCode = getCookieValue("revokeCode");
                 loadByRevokeCode(revokeCode)
-                    .then((response) => {
-                        response__ = response;
-                        if (!window.location.href.includes("!")) {
-                            window.location.replace("/!/");
-                        }
-                    })
-                    .catch((error) => {
-                        window.location.replace("https://xploit.men/aira/swiftlyid-error.php");
-                    });
+
             }
+
+        }
+        if(!window.location.href.includes("!")) {
+            window.location.replace("/!/");
         }
         return response__;
     }
+    
 }
