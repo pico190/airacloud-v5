276f49894920bb13051b4bd69e1dfa3a6dd6311a
pico190 <saulbuesosanz@gmail.com>
2023-12-15 22:47:27 +0100

    +cosa

--- src/utils/SwiftlyIDManager.js
+++ src/utils/SwiftlyIDManager.js
@@ -1,37 +1,45 @@
 import $ from 'jquery';
+import { useState } from 'react';
 // import { getCookieValue } from './GetCookieValue';
 
 export function StartSwiftlyIDClient() {
 
+    var [response, setResponse] = useState(undefined);
+
     const url = window.location.href;
+    const swiftlyIDError = "Error 12xGcidT9pwcVnsx3MXWVw$3R&j7W6";
 
-    if(!url.includes("/!/")) {
+    // urls
+    const getToken = "https://xploit.men/swiftly/api/swiftlyid/gettoken?revokeCode="
+    const getName = "https://xploit.men/swiftly/api/swiftlyid/getinfo/getname?token="
 
-        // Swiftly ID Not Started
-        if(url.includes("?revokeCode")) {
+    // Swiftly ID Not Started
+    if(url.includes("?revokeCode")) {
 
             // Revoke Code
             const revokeCode = url.split("?revokeCode=")[1];
-            var token = undefined
-            $.get(`https://xploit.men/swiftly/api/swiftlyid/gettoken?revokeCode=${revokeCode}`, (data) => {token = data;
+            $.get(getToken + revokeCode, (data) => {
+                
+                if(data === swiftlyIDError) {
+                    window.location.href = "https://xploit.men/aira/error.php?error=e0x01"
+                    return true;
+                }
             
-            alert(revokeCode + ", " + token)
-            })
+                var token = data;  
+                $.get(getName + token, (data) => {          
+                    window.history.pushState("", "", "/")
+                })                                                             
+
+                return true;                                                                                                 })
             
-
-        } else {
-
-            // Redirect
-            window.location.href = "https://xploit.men/swiftly/login/aira"
-        }
-
 
     } else {
 
-        // Swiftly ID Started
-
-
+            // Redirect
+            window.location.href = "https://xploit.men/swiftly/login/aira"
     }
+
+
 
 
 }
