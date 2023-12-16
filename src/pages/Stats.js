2d440df14cf9292a38f17ac4ab4988dc235c9033
pico190 <saulbuesosanz@gmail.com>
2023-12-16 16:46:03 +0100

    fkljñdfskl

--- src/pages/Stats.js
+++ src/pages/Stats.js
@@ -10,7 +10,7 @@ export function Stats() {
                 var CanvasJSChart = CanvasJSReact.CanvasJSChart;
                 
                 var chartWidth = window.innerWidth * 0.4;
-                var chartHeight = chartWidth / 2;
+                var chartHeight = chartWidth / 1.3;
 
                 const options = {
                     theme: "dark2",
