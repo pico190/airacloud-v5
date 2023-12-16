0cf9ecd5e3df38bd2bac3305dc3789292f257232
pico190 <saulbuesosanz@gmail.com>
2023-12-16 12:00:12 +0100

    hjkhfdjkfhs

--- src/utils/SwiftlyIDManager.js
+++ src/utils/SwiftlyIDManager.js
@@ -7,8 +7,8 @@ export function StartSwiftlyIDClient() {
     const swiftlyIDError = "Error 12xGcidT9pwcVnsx3MXWVw$3R&j7W6";
 
     // urls
-    const getToken = "https://xploit.men/swiftly/api/swiftlyid/gettoken?revokeCode="
-    const getName = "https://xploit.men/swiftly/api/swiftlyid/getinfo/getname?token="
+    const getToken = "https://xploit.men/swiftly/api/swiftlyid/gettoken?revokeCode=";
+    const getName = "https://xploit.men/swiftly/api/swiftlyid/getinfo/getname?token=";
 
     // Swiftly ID Not Started
     if(url.includes("?revokeCode")) {
@@ -25,22 +25,21 @@ export function StartSwiftlyIDClient() {
                 var responsetoken = data;  
                 $.get(getName + responsetoken, (data) => {          
                     
-                    // if(data === swiftlyIDError) {
-                    //     window.location.href = "https://xploit.men/aira/error.php?error=e0x01"
-                    //     return true;
-                    // }
+                    if(data === swiftlyIDError) {
+                        window.location.href = "https://xploit.men/aira/error.php?error=e0x01"
+                        return true;
+                    }
                     
                     // window.history.pushState("", "", "https://airacloud-v5.vercel.app/home/")
 
-                    // var lastresponse = {
-                    //     "usertoken": responsetoken,
-                    //     "username": data
-                    // }
+                    var lastresponse = {
+                        "usertoken": responsetoken,
+                        "username": data
+                    }
 
-                    // alert("jamon "+data);
+                    alert(lastresponse);
 
                     // lastresponse
-                    alert(data);
 
                 })                                                             
 
