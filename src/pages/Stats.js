f9797f5d65fbd37a2309494bc2e7506c493df955
pico190 <saulbuesosanz@gmail.com>
2023-12-16 13:30:37 +0100

    +statscss

--- src/pages/Stats.js
+++ src/pages/Stats.js
@@ -1,8 +1,15 @@
 import { NotFound } from './NotFound.js'
+import './statcs.css';
 
 export function Stats() {
     if(window.location.href.includes(`Fr0W7JpIHa0u5mRbCvJ0KDnP41J6qeZjdluvq2Exc3tDE6mRyl`)) {
-        return (<h1>Visitas</h1>)
+        return (
+            <>
+            <div className="stats">
+                <h1>Gráfico de visitas</h1>
+            </div>
+            </>
+        )
     } else {
         return (<NotFound />);
     }
