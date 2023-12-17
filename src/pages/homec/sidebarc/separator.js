4a823ebafb0278fe37a1453bc42052086e46e019
pico190 <saulbuesosanz@gmail.com>
2023-12-17 12:56:39 +0100

    addsfdsf

--- src/pages/homec/sidebarc/separator.js
+++ src/pages/homec/sidebarc/separator.js
@@ -9,17 +9,22 @@ export function SideBarSeparator() {
         function mouseup() {
             alert("usex");
         }
-
-        separator.removeEventListener("mousedown", mousedown);
-        separator.removeEventListener("mouseup", mouseup);
-        
             separator.addEventListener("mousedown", mousedown)
             separator.addEventListener("mouseup", mouseup)
     }
 
-    let m = 0
+    let m = 0;
 
-    setTimeout(() => {m +=1; loadSeparator(); alert(m)}, 700)
-    
+    const cambiarVariable = () => {
+        m += 1;
+        if(m===1) {
+            loadSeparator();
+            alert("asex")
+        }
+        alert(m)
+    };
+
+    setTimeout(() => { cambiarVariable(); }, 700);
+
     return <div id="sb-separator" className="sb-separator" />
 }
