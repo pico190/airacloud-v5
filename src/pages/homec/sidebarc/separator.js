b8702fbe9c4b5168b561db6566a2d915d9c42f32
pico190 <saulbuesosanz@gmail.com>
2023-12-17 13:05:55 +0100

    bardfix

--- src/pages/homec/sidebarc/separator.js
+++ src/pages/homec/sidebarc/separator.js
@@ -1,35 +1,26 @@
 export function SideBarSeparator() {
     
-        let m = 0;
-        let separatorInitialized = false;
-        
-        function loadSeparator() {
-            const separator = document.getElementById("sb-separator");
-        
-            function mousedown() {
-                alert("m");
-            }
-        
-            function mouseup() {
-                alert("u");
-            }
-        
-            separator.addEventListener("mousedown", mousedown);
-            separator.addEventListener("mouseup", mouseup);
-        
-            separatorInitialized = true; // Marcamos que el separador ha sido inicializado
-        }
-        
-        const cambiarVariable = () => {
-            m += 1;
-            if (m === 1 && !separatorInitialized) { // Verificamos si m es igual a 1 y si el separador no ha sido inicializado
-                loadSeparator();
-                alert("a");
-            }
-            alert(m);
-        };
-        
-        setTimeout(cambiarVariable, 700);
-        
-    return <div id="sb-separator" className="sb-separator" />
+let separatorInitialized = false; // Variable global
+
+function loadSeparator() {
+  const separator = document.getElementById("sb-separator");
+
+  function mousedown() {
+    alert("m");
+  }
+
+  function mouseup() {
+    alert("u");
+  }
+
+  if (!separatorInitialized) {
+    separator.addEventListener("mousedown", mousedown);
+    separator.addEventListener("mouseup", mouseup);
+
+    separatorInitialized = true; // Marcamos que el separador ha sido inicializado
+  }
 }
+
+loadSeparator();
+
+return <div id="sb-separator" className="sb-separator" />;}
