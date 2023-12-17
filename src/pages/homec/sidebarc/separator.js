969f6ad13089221f9de9cf1271d81ed0674d2884
pico190 <saulbuesosanz@gmail.com>
2023-12-17 12:59:24 +0100

    gptfix

--- src/pages/homec/sidebarc/separator.js
+++ src/pages/homec/sidebarc/separator.js
@@ -1,30 +1,35 @@
 export function SideBarSeparator() {
     
-    function loadSeparator() {
-        var separator = document.getElementById("sb-separator");
+        let m = 0;
+        let separatorInitialized = false;
         
-        function mousedown() {
-            alert("msex");
+        function loadSeparator() {
+            const separator = document.getElementById("sb-separator");
+        
+            function mousedown() {
+                alert("m");
+            }
+        
+            function mouseup() {
+                alert("u");
+            }
+        
+            separator.addEventListener("mousedown", mousedown);
+            separator.addEventListener("mouseup", mouseup);
+        
+            separatorInitialized = true; // Marcamos que el separador ha sido inicializado
         }
-        function mouseup() {
-            alert("usex");
-        }
-            separator.addEventListener("mousedown", mousedown)
-            separator.addEventListener("mouseup", mouseup)
-    }
-
-    let m = 0;
-
-    const cambiarVariable = () => {
-        m += 1;
-        if(m===1) {
-            loadSeparator();
-            alert("asex")
-        }
-        alert(m)
-    };
-
-    setTimeout(() => { cambiarVariable(); }, 700);
-
+        
+        const cambiarVariable = () => {
+            m += 1;
+            if (m === 1 && !separatorInitialized) { // Verificamos si m es igual a 1 y si el separador no ha sido inicializado
+                loadSeparator();
+                alert("a");
+            }
+            alert(m);
+        };
+        
+        setTimeout(cambiarVariable, 700);
+        
     return <div id="sb-separator" className="sb-separator" />
 }
