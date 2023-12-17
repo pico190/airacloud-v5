21d9aa3b53814a4404a44d266b28240fae3ad13b
pico190 <saulbuesosanz@gmail.com>
2023-12-17 13:07:34 +0100

    bardfix2

--- src/pages/homec/sidebarc/separator.js
+++ src/pages/homec/sidebarc/separator.js
@@ -1,26 +1,37 @@
 export function SideBarSeparator() {
-    
-let separatorInitialized = false; // Variable global
-
-function loadSeparator() {
-  const separator = document.getElementById("sb-separator");
-
-  function mousedown() {
-    alert("m");
-  }
-
-  function mouseup() {
-    alert("u");
-  }
-
-  if (!separatorInitialized) {
-    separator.addEventListener("mousedown", mousedown);
-    separator.addEventListener("mouseup", mouseup);
-
-    separatorInitialized = true; // Marcamos que el separador ha sido inicializado
-  }
+    class Separator extends React.Component {
+        state = {
+          separatorInitialized: false,
+        };
+      
+        componentDidMount() {
+          if (!this.state.separatorInitialized) {
+            this.loadSeparator();
+          }
+        }
+      
+        loadSeparator() {
+          const separator = document.getElementById("sb-separator");
+      
+          function mousedown() {
+            alert("m");
+          }
+      
+          function mouseup() {
+            alert("u");
+          }
+      
+          separator.addEventListener("mousedown", mousedown);
+          separator.addEventListener("mouseup", mouseup);
+      
+          this.setState({
+            separatorInitialized: true,
+          }); // Marcamos que el separador ha sido inicializado
+        }
+      
+        render() {
+          return <div id="sb-separator" className="sb-separator" />;
+        }
+      }
+      
 }
-
-loadSeparator();
-
-return <div id="sb-separator" className="sb-separator" />;}
