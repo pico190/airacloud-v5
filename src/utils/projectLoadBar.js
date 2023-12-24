f0aeddf828b9f2723a9ffbe3d2498b0756b01c3c
pico190 <saulbuesosanz@gmail.com>
2023-12-24 09:29:46 +0100

    animation1

--- src/utils/projectLoadBar.js
+++ src/utils/projectLoadBar.js
@@ -1,5 +1,12 @@
 export function projectLoadBar() {
 
-    document.body.innerHTML = document.body.innerHTML + '<div id="airaloadbar"><h1>sex</h1></div>'
+    document.body.innerHTML = document.body.innerHTML + '<div id="airaloadbar" style="left: 100vw;"><h1>sex</h1></div>'
+    var airaloadbar = document.getElementById("airaloadbar");
+    var root = document.getElementById("root");
+    root.style.left = "0vw";
+    setTimeout(() => {
+        airaloadbar.style.root = "-100vw";
+        airaloadbar.style.left = "0vw";
+    }, 200)
 
 }
