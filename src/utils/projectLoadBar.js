ca71889048822a62727b238253fc0a7767331608
pico190 <saulbuesosanz@gmail.com>
2023-12-24 10:15:57 +0100

    dgfdfhd

--- src/utils/projectLoadBar.js
+++ src/utils/projectLoadBar.js
@@ -1,6 +1,6 @@
 export function projectLoadBar() {
 
-    document.body.innerHTML = document.body.innerHTML + '<div id="airaloadbar" style="left: 100vw;"><img src="https://airacloud-v5.vercel.app/favicon.ico" alt="" loading="lazy"><h1>AiraCloud</h1></div>'
+    document.body.innerHTML = document.body.innerHTML + '<div id="airaloadbar" style="left: 100vw;"><font><img src="https://airacloud-v5.vercel.app/favicon.ico" alt="" loading="lazy"><h1>AiraCloud</h1></font><span></span></div>'
     var airaloadbar = document.getElementById("airaloadbar");
     var root = document.getElementById("root");
     root.style.left = "0vw";
