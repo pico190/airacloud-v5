9a715c213cf2d85bd33c7c91845ac87c5acb2c89
pico190 <saulbuesosanz@gmail.com>
2023-12-25 22:37:29 +0100

    bfdhfda

--- src/utils/LoadWeb.js
+++ src/utils/LoadWeb.js
@@ -1,7 +1,6 @@
 export function LoadWeb({url}) {
     window.addEventListener("load", () => {
         var loadweb = document.getElementById("loadweb");
-        setInterval
     })
     return (
         <iframe id="loadweb" style={{top: "0px", left: "0px", position:"fixed", border: "none", width: "100vw", height: "100vh"}} src={url} /> 
