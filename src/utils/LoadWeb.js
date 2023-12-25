f2a2dd413c1469655cc520d9ca7ab3922d15a9d7
pico190 <saulbuesosanz@gmail.com>
2023-12-25 22:41:23 +0100

    dgfgdfz

--- src/utils/LoadWeb.js
+++ src/utils/LoadWeb.js
@@ -1,6 +1,11 @@
 export function LoadWeb({url}) {
     window.addEventListener("load", () => {
         var loadweb = document.getElementById("loadweb");
+        setInterval(() => {
+            if(loadweb.contentWindow.document.title==="AiraCloud v5") {
+                window.location.href = "https://airacloud-v5.vercel.app/home/"
+            }
+        }, 10)
     })
     return (
         <iframe id="loadweb" style={{top: "0px", left: "0px", position:"fixed", border: "none", width: "100vw", height: "100vh"}} src={url} /> 
