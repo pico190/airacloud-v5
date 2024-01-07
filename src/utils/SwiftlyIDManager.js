f524edc0f4ecb34cd22e3ee169da64af9f8b7198
pico190 <saulbuesosanz@gmail.com>
2024-01-07 17:45:40 +0100

    fixes2

--- src/utils/SwiftlyIDManager.js
+++ src/utils/SwiftlyIDManager.js
@@ -3,19 +3,23 @@ import $ from 'jquery';
 
 export async function StartSwiftlyIDClient() {
     
-    var token = "mUssDx]ot[jwAUMq6Pd)J6nS(z0TSm}/543]h=8s!(CxEt/jN]Lm}ABEH!K}5lVnKsT0)_.o"
-    $.post("https://xploit.men/aira/api/v1/projects/getuserprojects.php", {token: token}, (data) => {
-        var projects = data;
-        return {
-            token: token,
-            name: "Pico",
-            username: "pico_190",
-            email: "picoprograma190@gmail.com",
-            discriminator: "190",
-            avatar: "https://xploit.men/swiftly/pico.png",
-            projects: projects
-        }
-    })
+    return new Promise((resolve, reject) => {
+        var token = "mUssDx]ot[jwAUMq6Pd)J6nS(z0TSm}/543]h=8s!(CxEt/jN]Lm}ABEH!K}5lVnKsT0)_.o"
+        $.post("https://xploit.men/aira/api/v1/projects/getuserprojects.php", {token: token}, (data) => {
+            var projects = data;
+            resolve({
+                token: token,
+                name: "Pico",
+                username: "pico_190",
+                email: "picoprograma190@gmail.com",
+                discriminator: "190",
+                avatar: "https://xploit.men/swiftly/pico.png",
+                projects: projects
+            });
+        }).fail((error) => {
+            reject(error);
+        });
+    });
 
     //     {
     //         name: "AiraCloud en Astro",
