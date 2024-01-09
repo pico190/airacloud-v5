9906c7a013de8fc0d56e9938292d77fb37664b08
pico190 <saulbuesosanz@gmail.com>
2024-01-09 17:51:54 +0100

    gptfix

--- src/utils/translates/text.js
+++ src/utils/translates/text.js
@@ -1,38 +1,36 @@
 export function Text({ id }) {
-
-    var language = navigator.language.slice(0,2);
-
+    var language = navigator.language.slice(0, 2);
 
     if (!document.cookie.includes("DATA__LANG")) {
-        
- 
+    
         let storedData = localStorage.getItem('langs');
-
+    
         if (storedData) {
             processData(JSON.parse(storedData));
         } else {
             fetch('https://airacloud-v5.vercel.app/translates/langs.json')
-            .then(response => response.json())
-            .then(data => {
-                localStorage.setItem('langs', JSON.stringify(data));
-                processData(data);
-            })
-            .catch(error => {
-                console.error("Error to load text. Please check your internet connection");
-            });
+                .then(response => response.json())
+                .then(data => {
+                    localStorage.setItem('langs', JSON.stringify(data));
+                    processData(data, language);
+                })
+                .catch(error => {
+                    console.error("Error to load text. Please check your internet connection");
+                });
         }
-
-        function processData(data) {
-            if (!data.includes(language)) {
+    
+        function processData(data, language) {
+            let languages = Object.keys(data);
+            if (!languages.includes(language)) {
                 language = "en";
             }
+            
+            let currentDate = new Date();
+            let expiryDate = new Date(currentDate.getFullYear() + 1, currentDate.getMonth(), currentDate.getDate());
+    
+            document.cookie = "DATA__LANG=" + language + "; SameSite=Strict; Secure; path=/; expires=" + expiryDate.toUTCString() + ";"
+            document.cookie = "DATA__LANG=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
         }
-
-
-        let currentDate = new Date();
-        let expiryDate = new Date(currentDate.getFullYear() + 1, currentDate.getMonth(), currentDate.getDate());
-
-        document.cookie = "DATA__LANG="+language+"; SameSite=Strict; Secure; path=/; expires="+expiryDate.toUTCString()+";"
-        document.cookie = "DATA__LANG=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
     }
+    
 }
