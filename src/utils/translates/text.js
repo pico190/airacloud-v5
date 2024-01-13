8098283a4a228d837e02f07fd87d8ddb3c07695a
pico190 <saulbuesosanz@gmail.com>
2024-01-13 19:40:46 +0100

    testfix

--- src/utils/translates/text.js
+++ src/utils/translates/text.js
@@ -5,16 +5,19 @@ export function Text({ id, var1="" }) {
     const currentDate = new Date();
     const expiryDate = new Date(currentDate.getFullYear() + 1, currentDate.getMonth(), currentDate.getDate());
 
-    const langCookie = gcookie("DATA__LANG");
-    const storedLangs = localStorage.getItem('langs');
-    
-    if (!langCookie || (storedLangs && !Object.keys(JSON.parse(storedLangs)).includes(langCookie))) {
+    let langCookie = gcookie("DATA__LANG");
+    let storedLang = localStorage.getItem('selectedLang');
+
+    if (!langCookie || (storedLang && langCookie !== storedLang)) {
         fetch('https://airacloud-v5.vercel.app/translates/langs.json')
             .then(response => response.json())
             .then(data => {
                 const languages = Object.keys(data);
                 const selectedLang = languages.includes(language) ? language : "en";
                 localStorage.setItem('langs', JSON.stringify(data));
+                localStorage.setItem('selectedLang', selectedLang);
+                langCookie = selectedLang;
+                document.cookie = `DATA__LANG=${langCookie}; SameSite=Strict; Secure; path=/; expires=${expiryDate.toUTCString()};`;
                 fetchAndSaveTranslates(selectedLang);
             })
             .catch(error => console.error("Error loading text. Check your internet connection"));
