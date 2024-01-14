b95d139abe9c5ec5a76dd386c50e9cef3531daf4
pico190 <saulbuesosanz@gmail.com>
2024-01-14 16:33:48 +0100

    addDG

--- src/pages/Editor.js
+++ src/pages/Editor.js
@@ -31,7 +31,7 @@ $a = "patatas";
             theme="vs-dark" 
             defaultLanguage="php" 
             defaultValue={testPhpCode} />
-            <div style={{position:"fixed", right: "0px", height: "100vh", width: "20%"}}>
+            <div style={{position:"fixed", right: "0px", height: "100vh", width: "20%", padding: "8px", top: "0px"}}>
                 <b>Propiedades epicas del editor</b><span> (esto es de aira y es temporal papi)</span>
                 <hr />
                 <input value="Consolas" onChange={() => {changeFont()}} id="fontFamily" />
