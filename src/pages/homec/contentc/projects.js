5a374792d0d2db5df9d59da5ac826eb71276bcca
pico190 <saulbuesosanz@gmail.com>
2024-01-14 10:44:14 +0100

    testprojectsautomatization

--- src/pages/homec/contentc/projects.js
+++ src/pages/homec/contentc/projects.js
@@ -1,44 +1,52 @@
 
 export function Projects() {
+    var alluserprojects = JSON.parse(decode(gcookie("DATA__PROJECTS")));
     var rows = 4;
     var columns = 5;
-
-
+    var projectBox = <div class="project-box">{
+        Array.from({ length: rows }, (_, rowIndex) => (
+            <div class="row" key={rowIndex}>
+                {
+                    Array.from({ length: columns }, (_, colIndex) => (
+                        <div class="item item-fill" key={colIndex} />
+                    ))
+                }
+            </div>
+        ))
+    }</div>;
+    
     return (
         <>
-            <div class="project-box">
-                {
-                    projectBox
-                }
-                {/* <div class="row">
-                    <div class="item">potatoes</div>
-                    <div class="item">potatoes</div>
-                    <div class="item">potatoes</div>
-                    <div class="item">potatoes</div>
-                    <div class="item">potatoes</div>
-                </div>
-                <div class="row">
-                    <div class="item">potatoes</div>
-                    <div class="item item-fill" />
-                    <div class="item item-fill" />
-                    <div class="item item-fill" />
-                    <div class="item item-fill" />
-                </div>
-                <div class="row">
-                    <div class="item item-fill" />
-                    <div class="item item-fill" />
-                    <div class="item item-fill" />
-                    <div class="item item-fill" />
-                    <div class="item item-fill" />
-                </div>
-                <div class="row">
-                    <div class="item item-fill" />
-                    <div class="item item-fill" />
-                    <div class="item item-fill" />
-                    <div class="item item-fill" />
-                    <div class="item item-fill" />
-                </div> */}
-            </div>
+            {projectBox}
         </>
-    )
+    );
+    
+                /* <div class="row">
+                    <div class="item">potatoes</div>
+                    <div class="item">potatoes</div>
+                    <div class="item">potatoes</div>
+                    <div class="item">potatoes</div>
+                    <div class="item">potatoes</div>
+                </div>
+                <div class="row">
+                    <div class="item">potatoes</div>
+                    <div class="item item-fill" />
+                    <div class="item item-fill" />
+                    <div class="item item-fill" />
+                    <div class="item item-fill" />
+                </div>
+                <div class="row">
+                    <div class="item item-fill" />
+                    <div class="item item-fill" />
+                    <div class="item item-fill" />
+                    <div class="item item-fill" />
+                    <div class="item item-fill" />
+                </div>
+                <div class="row">
+                    <div class="item item-fill" />
+                    <div class="item item-fill" />
+                    <div class="item item-fill" />
+                    <div class="item item-fill" />
+                    <div class="item item-fill" />
+                </div> */
 }
