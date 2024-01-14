6f7c26f2967cd5db6c0e879f54d9b806765835fd
pico190 <saulbuesosanz@gmail.com>
2024-01-14 15:54:42 +0100

    monaco? areyouthere?

--- src/pages/Editor.js
+++ src/pages/Editor.js
@@ -1,8 +1,19 @@
+import React from 'react';
+
 import { Content } from './homec/Content'
 import { SideBar } from './homec/Sidebar'
+import { LoadWeb } from '../utils/LoadWeb'
+
+import Monaco from '@monaco-editor/react';
 
 export function Editor({urlparsed, sidinfo}) {
-    return (
-        <h1>{urlparsed[1]}</h1>
-    )
+    if(urlparsed[1].length<=0) {
+        return (
+            <LoadWeb url="https://airacloud-v5.vercel.app/notfound.html" />
+        )
+    } else {
+        return (
+            <Monaco height="100px" defaultLanguage="javascript" defaultValue="// some comment" />
+        )
+    }
 }
