22d07783c4c9319679c03ca3b3424c4fb6006606
pico190 <saulbuesosanz@gmail.com>
2024-01-14 17:02:01 +0100

    optiontry

--- src/pages/Editor.js
+++ src/pages/Editor.js
@@ -29,12 +29,13 @@ $a = "patatas";
             width="80%"
             onLoad={() => {alert("sex")}}
             theme="vs-dark" 
+            contextmenu={false}
             defaultLanguage="php" 
             defaultValue={testPhpCode} />
             <div style={{position:"fixed", right: "0px", height: "100vh", width: "20%", padding: "8px", top: "0px", backgroundColor: "#ffffff08"}}>
                 <b>Propiedades epicas del editor</b><span> (esto es de aira y es temporal papi)</span>
                 <hr style={{border: 0, borderBottom: "1px solid #ffffff24"}} />
-                <input value="Consolas" placeholder="Font Family" onChange={() => {changeFont()}} id="fontFamily" style={{background: "#ffffff0f", border: 0, padding: "8px", borderRadius: "3px"}} />
+                <input value="Consolas" placeholder="Font Family" onChange={() => {changeFont()}} id="fontFamily" style={{background: "#ffffff0f", border: 0, padding: "8px", borderRadius: "3px", outline: 0}} />
             </div>
             </>
         )
