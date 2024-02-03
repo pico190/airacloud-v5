72fabd90f72fcb331f0f33d9b1806ee0b21dc48f
pico190 <saulbuesosanz@gmail.com>
2024-02-03 17:54:52 +0100

    textdeploy

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { Analytics } from '@vercel/analytics/react';
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 ReactDOM.render(
   <React.StrictMode>
