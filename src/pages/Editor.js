4f1806207cee6a5938dad09ec183e05da9ca0d82
pico190 <saulbuesosanz@gmail.com>
2024-02-18 15:49:56 +0100

    fixes

--- src/pages/Editor.js
+++ src/pages/Editor.js
@@ -40,12 +40,14 @@ export function Editor({urlparsed, sidinfo}) {
                 <div style={{display: "flex", gap: "10px"}}>
                     <img src={'https://'+window.location.host+'/airaicons/'+urlparsed[1]+'.svg'} loading="lazy" alt="" style={{width: "20px"}} />
                     <span>{urlparsed[1]}</span>
+                    <div style={{display: "flex", gap: "10px", flexDirection: "colum"}}>
                         <hr />
                         <b>Font Size:</b>
                         <input type="range" id="fontsizeinput" min="10" max="55" value="20" onChange={() => {document.getElementById("fontsize").innerHTML=`.cm-editor, .cm-editor * {font-size: ${document.getElementById("fontsizeinput").value}px;}`}} />
                         <br />
                         <b>Font Family:</b>
                         <input type="text" id="fontfamilyinput" value="Consolas" placeholder="Introduce una fuente que esté en tu dispositivo." onChange={() => {document.getElementById("fontfamily").innerHTML=`.cm-editor, .cm-editor * {font-family: ${document.getElementById("fontfamilyinput").value}px;}`}} style={{border: "none", outline: 0, color: "black"}} />
+                    </div>
                 </div>
             </SideBar>
             <div className="content">
