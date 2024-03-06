7d1f562d150ab9042751ee0936504b65f271b6d2
pico190 <saulbuesosanz@gmail.com>
2024-03-06 09:57:24 +0100

    superdooperfixed

--- src/pages/Editor.js
+++ src/pages/Editor.js
@@ -129,7 +129,7 @@ export function Editor({urlparsed, sidinfo}) {
         })
         setInterval(() => {
 
-            Array.from(document.getElementsByClassName("cm-selectionBackground")).forEach(elem => { elem.style.background = "#243047!important" }) 
+            Array.from(document.getElementsByClassName("cm-selectionBackground")).forEach(elem => { elem.style.setProperty('background', '#243047', 'important'); }) 
             Array.from(document.querySelectorAll('span[title="Fold line"]')).forEach(elem => {elem.innerHTML=`<img src="https://xploit.men/scdn/?fluenticons&name=chevron-down" alt="v" loading="lazy">`})
             Array.from(document.querySelectorAll('span[title="Unfold line"]')).forEach(elem => {elem.innerHTML=`<img src="https://xploit.men/scdn/?fluenticons&name=chevron-right" alt=">" loading="lazy">`})
 
