98a0d15371e2ec9e4bd27a42f4c902802b8934d3
pico190 <saulbuesosanz@gmail.com>
2024-03-07 12:07:24 +0100

    markdown

--- src/pages/Editor.js
+++ src/pages/Editor.js
@@ -6,11 +6,18 @@ import { SideBar } from './editorc/Sidebar'
 import { encode, decode } from 'js-base64'
 
 import CodeMirror from '@uiw/react-codemirror';
-import { javascript } from '@codemirror/lang-javascript';
+
 import { html } from '@codemirror/lang-html';
+import { java } from '@codemirror/lang-java';
+import { javascript } from '@codemirror/lang-javascript';
+import { json } from '@codemirror/lang-json';
+import { php } from '@codemirror/lang-php';
+import { sql } from '@codemirror/lang-sql';
+import { markdown } from '@codemirror/lang-markdown';
+
 import { color } from '@uiw/codemirror-extensions-color';
 import { spaces } from "../utils/generalfuncs";
-// import { hyperLink } from '@uiw/codemirror-extensions-hyper-link';
+import { hyperLink } from '@uiw/codemirror-extensions-hyper-link';
 
 import { CmenuElement, ContextMenu } from "../utils/contextmenu";
 
@@ -51,7 +58,6 @@ export function Editor({urlparsed, sidinfo}) {
         const [intelliContent, setIntelliContent] = useState([]);
         const [intelliDesc, setIntelliDesc] = useState('');
         const [ intelliloaded, loadIntelli ] = useState(false); // eslint-disable-next-line
-        const [ codeValue, setCodeValue ] = useState(code); // eslint-disable-next-line
         var [ options, setOptions ] = useState({
             refreshMode: refreshMode.delay,
         }); // eslint-disable-next-line
@@ -208,6 +214,125 @@ export function Editor({urlparsed, sidinfo}) {
             return () => clearTimeout(timer);
         }, [editorToken]);
     
+        /*
+        * default html: --- html({ config:{matchClosingTags: true, autoCloseTags: true } })
+        * default jsx: ---
+        * 
+        */
+
+
+
+
+        const [value, setValue] = React.useState(`# Header 1 #
+        ## Header 2 ##
+        ### Header 3 ###             (Hashes on right are optional)
+        ## Markdown plus h2 with a custom ID ##   {#id-goes-here}
+        [Link back to H2](#id-goes-here)
+        
+        \`\`\`js
+        var x = "string";
+        function f() {
+          return x;
+        }
+        \`\`\`
+        
+        <!-- html madness -->
+        <div class="custom-class" markdown="1">
+          <div>
+            nested div
+          </div>
+          <script type='text/x-koka'>
+            function( x: int ) { return x*x; }
+          </script>
+          This is a div _with_ underscores
+          and a & <b class="bold">bold</b> element.
+          <style>
+            body { font: "Consolas" }
+          </style>
+        </div>
+        
+        * Bullet lists are easy too
+        - Another one
+        + Another one
+        
+        This is a paragraph, which is text surrounded by
+        whitespace. Paragraphs can be on one
+        line (or many), and can drone on for hours.
+        
+        Now some inline markup like _italics_,  **bold**,
+        and \`code()\`. Note that underscores
+        in_words_are ignored.
+        
+        \`\`\`\`application/json
+          { value: ["or with a mime type"] }
+        \`\`\`\`
+        
+        > Blockquotes are like quoted text in email replies
+        >> And, they can be nested
+        
+        1. A numbered list
+        2. Which is numbered
+        3. With periods and a space
+        
+        And now some code:
+        
+            // Code is just text indented a bit
+            which(is_easy) to_remember();
+        
+        And a block
+        
+        ~~~
+        // Markdown extra adds un-indented code blocks too
+        
+        if (this_is_more_code == true && !indented) {
+            // tild wrapped code blocks, also not indented
+        }
+        ~~~
+        
+        Text with
+        two trailing spaces
+        (on the right)
+        can be used
+        for things like poems
+        
+        ### Horizontal rules
+        
+        * * * *
+        ****
+        --------------------------
+        
+        ![picture alt](/images/photo.jpeg "Title is optional")
+        
+        ## Markdown plus tables ##
+        
+        | Header | Header | Right  |
+        | ------ | ------ | -----: |
+        |  Cell  |  Cell  |   $10  |
+        |  Cell  |  Cell  |   $20  |
+        
+        * Outer pipes on tables are optional
+        * Colon used for alignment (right versus left)
+        
+        ## Markdown plus definition lists ##
+        
+        Bottled water
+        : $ 1.25
+        : $ 1.55 (Large)
+        
+        Milk
+        Pop
+        : $ 1.75
+        
+        * Multiple definitions and terms are possible
+        * Definitions can include multiple paragraphs too
+        
+        *[ABBR]: Markdown plus abbreviations (produces an <abbr> tag)`);
+        const onChange = React.useCallback((val, viewUpdate) => {
+          console.log('val:', val);
+          setValue(val);
+        }, []);
+      
+
         return (
             <>
 
@@ -237,12 +362,13 @@ export function Editor({urlparsed, sidinfo}) {
                             <div id="intellidesc" dangerouslySetInnerHTML={{__html: intelliDesc}}></div>
                         </div> */}
                         <CodeMirror
-                            extensions={[html({ config:{matchClosingTags: true, autoCloseTags: true } }), color]}
-                            value={codeValue}
+                            extensions={[markdown(), hyperLink, color]}
+                            value={value}
                             theme={airatheme}
                             options={{
                                 autocompletion: false
                             }}
+                            onChange={onChange}
                         />
                         <div className="cm-info" id="cm-info" children="Download IntelliSense..." />
                     </div>
--- src/utils/codemirror/airatheme.js
+++ src/utils/codemirror/airatheme.js
@@ -16,7 +16,6 @@ export const airatheme = createTheme({
       selectionMatch:         cssvar("selection-match"),
       gutterBackground:       cssvar("gutter-bg"),
       gutterForeground:       cssvar("gutter-fg"),
-      gutterBorder:           cssvar("gutter-border"),
       gutterActiveForeground: cssvar("gutter-active-foreground"),
       lineHighlight:          cssvar("lineHighlight"),
     },
