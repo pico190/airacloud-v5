cb30ae38188a553ac90d0539b1c16f69d15c1cb0
pico190 <saulbuesosanz@gmail.com>
2024-03-08 14:48:28 +0100

    pongamoslo a prueba

--- src/pages/homec/contentc/projects.js
+++ src/pages/homec/contentc/projects.js
@@ -1,7 +1,7 @@
 import { decode } from 'js-base64';
 import React, {useState} from 'react';
 import {DndContext, closestCenter} from '@dnd-kit/core';
-import {SortableContext, rectSortingStrategy} from '@dnd-kit/sortable';
+import {SortableContext, rectSortingStrategy, arrayMove} from '@dnd-kit/sortable';
 import { Project } from './project'
 export function Projects() {
 
@@ -23,32 +23,21 @@ export function Projects() {
         
     }
 
-    var alluserprojects = JSON.parse(decode(localStorage.getItem("DATA__PROJECTS")));
-    alluserprojects.push({name: "add", id: "add"})
+    var prjscts=JSON.parse(decode(localStorage.getItem("DATA__PROJECTS")))
+    prjscts.push({name: "add", id: "add"})
+    var [alluserprojects, setuserprojects] = useState(prjscts)
 
-        // alluserprojects.map(item => {
-        //     const itemName = item.name || "";
-        //     if (itemName !== "add") {
-        //         const itemIconName = item.type || "txt";
-        //         const itemIconSrc = `https://${window.location.host}/airaicons/${itemIconName}.svg`;
-        //         const isFill = itemName === "" ? " item-fill" : "";
-        //         const itemIcon = isFill.includes("item-fill") ? null : <img height="86" src={itemIconSrc} loading="lazy" alt=""/>;
-        //         return (
-        //             <div className={"item animation-box" + isFill} key={item.id}>
-        //                 {itemIcon}
-        //                 <b>{itemName}</b>
-        //             </div>
-        //         );
-        //     } else {
-        //         return (
-        //             <div className="item item-fill item-add animation-box" key={item.id}>
-        //                 <img height="60" src="https:xploit.men/scdn/?fluenticons&name=add" loading="lazy" alt="Crear proyecto" />
-        //             </div>
-        //         );
-        //     }
-        // });
         
-    const handleDragEnd = () => {console.log("drag end")}
+    const handleDragEnd = (event) => {
+        const {active, over} = event
+        const oldIndex = alluserprojects.findIndex(project => project.id === active.id)
+        const newIndex = alluserprojects.findIndex(project => project.id === over.id)
+
+        const newOrder = arrayMove(alluserProject, oldIndex, newIndex)
+        setuserprojects(newOrder)
+
+        // solicitud post
+    }
     return (
 
         <div onLoad={() => {animation();}} className="project-grid">
@@ -60,7 +49,7 @@ export function Projects() {
                 items={alluserprojects}
                 strategy={rectSortingStrategy}
                 activationConstraint={{
-                    delay: 150,
+                    delay: 250,
                     tolerance: 5,
                 }}
             >
@@ -74,67 +63,4 @@ export function Projects() {
         </div>
     );
 
-//     var rows = 4;
-//     var columns = 5;
-//     var projectBox = <div className="project-box">{
-//     Array.from({ length: rows }, (_, rowIndex) => (
-//         <div className="row" key={rowIndex}>
-//             {
-//                 Array.from({ length: columns }, (_, colIndex) => {
-//                     const index = rowIndex * columns + colIndex;
-//                     const item =  alluserprojects[index] || {};
-//                     const itemName = item.name || "";
-//                     if(itemName!=="add") {
-
-//                         var itemIconName = item.type || "txt";
-//                         var itemIcon = <img height="86" src={"https://"+window.location.host+"/airaicons/" + itemIconName + ".svg"} loading="lazy" alt=""/>;
-//                         var isFill = itemName === "" ? " item-fill" : "";
-//                         isFill.includes("item-fill") === true ? itemIcon = <></> : void(0);
-//                         return <div className={"item" + isFill} key={colIndex}>
-//                             {itemIcon}
-//                             <b>{itemName}</b>
-//                            </div>;
-//                     } else {
-//                         return (
-//                             <div className="item item-fill item-add" key={colIndex}>
-//                             <img height="60" src="https://xploit.men/scdn/?fluenticons&name=add" loading="lazy" alt="Crear proyecto" />
-//                            </div>
-//                         )
-//                     }
-//                 })
-//             }
-//         </div>
-//     ))
-// }</div>;
-
-
-    
-                /* <div className="row">
-                    <div className="item">potatoes</div>
-                    <div className="item">potatoes</div>
-                    <div className="item">potatoes</div>
-                    <div className="item">potatoes</div>
-                    <div className="item">potatoes</div>
-                </div>
-                <div className="row">
-                    <div className="item">potatoes</div>
-                    <div className="item item-fill" />
-                    <div className="item item-fill" />
-                    <div className="item item-fill" />
-                    <div className="item item-fill" />
-                </div>
-                <div className="row">
-                    <div className="item item-fill" />
-                    <div className="item item-fill" />
-                    <div className="item item-fill" />
-                    <div className="item item-fill" />
-                    <div className="item item-fill" />
-                </div>
-                <div className="row">
-                    <div className="item item-fill" />
-                    <div className="item item-fill" />
-                    <div className="item item-fill" />
-                    <div className="item item-fill" />
-                    <div className="item item-fill" />
-                </div> */
 }
