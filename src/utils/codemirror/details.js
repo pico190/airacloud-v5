fcd5ca14bdfc8141fa7399d5a374b60d12737f56
pico190 <saulbuesosanz@gmail.com>
2024-03-24 16:41:46 +0100

    mditalic

--- src/utils/codemirror/details.js
+++ src/utils/codemirror/details.js
@@ -19,13 +19,21 @@ export function loadDetails() {
                 }
             })
 
-            
             document.querySelectorAll("span").forEach(span => {
                     var spanstyle = window.getComputedStyle(span);
                     var cssvarjson = hexToRgb(spanstyle.getPropertyValue('--cm-md-bold'))
                     var cssvar = `rgb(${cssvarjson.r}, ${cssvarjson.g}, ${cssvarjson.b})`
                     if(cssvar === spanstyle.color) {
                         span.classList.add("bold")
+                    }
+            })
+            
+            document.querySelectorAll("span").forEach(span => {
+                    var spanstyle = window.getComputedStyle(span);
+                    var cssvarjson = hexToRgb(spanstyle.getPropertyValue('--cm-md-emphasis'))
+                    var cssvar = `rgb(${cssvarjson.r}, ${cssvarjson.g}, ${cssvarjson.b})`
+                    if(cssvar === spanstyle.color) {
+                        span.classList.add("italic")
                     }
             })
 }
