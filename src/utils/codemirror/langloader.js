dc29665f06a14c9593cceed5f71c376bea8e06f6
pico190 <saulbuesosanz@gmail.com>
2024-03-24 12:35:05 +0100

    Added language support: + java + swift + kotlin + sql + lua
    Fixed languages:
    - jsx
    - typescript
    - tsx

--- src/utils/codemirror/langloader.js
+++ src/utils/codemirror/langloader.js
@@ -5,14 +5,19 @@ export default function langLoader(lang, langs) {
     if (lang === "html") { lng = langs.html({ config: { matchClosingTags: true, autoCloseTags: true } }) }
     else if (lang === "css") { lng = langs.less() }
     else if (lang === "js") { lng = langs.javascript() }
-    else if (lang === "jsx") { lng = langs.javascript({ config: { jsx: true } }) }
-    else if (lang === "ts") { lng = langs.javascript({ config: { typescript: true } }) }
-    else if (lang === "tsx") { lng = langs.javascript({ config: { typescript: true, jsx: true } }) }
+    else if (lang === "jsx") { lng = langs.jsx() }
+    else if (lang === "ts") { lng = langs.typescript() }
+    else if (lang === "tsx") { lng = langs.tsx() }
     else if (lang === "markdown") { lng = langs.markdown() }
     else if (lang === "python") { lng = langs.python() }
     else if (lang === "json") { lng = langs.json() }
     else if (lang === "php") { lng = langs.php() }
     else if (lang === "java") { lng = langs.java() }
+    else if (lang === "swift") { lng = langs.swift() }
+    else if (lang === "kotlin") { lng = langs.kotlin() }
+    else if (lang === "sql") { lng = langs.sql() }
+    else if (lang === "rust") { lng = langs.rust() }
+    else if (lang === "lua") { lng = langs.lua() }
     else { lng = null }
     return lng;
 }
