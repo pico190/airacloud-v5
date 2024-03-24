22118aacc9d6e24f447b754ee08f9a8e74d938b5
pico190 <saulbuesosanz@gmail.com>
2024-03-24 13:25:34 +0100

    details duplicate

--- src/pages/Editor-nocookie.js
+++ src/pages/Editor-nocookie.js
@@ -12,6 +12,7 @@ import { airatheme } from "../utils/codemirror/airatheme";
 import $ from 'jquery'
 import { spaces, hexToRgb } from "../utils/generalfuncs";
 import langLoader from "../utils/codemirror/langloader";
+import { loadDetails } from "../utils/codemirror/details";
 
 export function EditorNoCookie({ urlparsed }) {
     const [reference, setReference] = useState([]);
@@ -21,24 +22,7 @@ export function EditorNoCookie({ urlparsed }) {
     const [extensionsarray, setExtensions] = useState(null);
     
     setInterval(() => {
-            
-        // Details
-        Array.from(document.getElementsByClassName("cm-selectionBackground")).forEach(elem => { elem.style.setProperty('background', '#243047', 'important'); }) 
-        Array.from(document.querySelectorAll('span[title="Fold line"]')).forEach(elem => {elem.innerHTML=`<img src="https://xploit.men/scdn/?fluenticons&name=chevron-down" alt="v" loading="lazy">`})
-        Array.from(document.querySelectorAll('span[title="Unfold line"]')).forEach(elem => {elem.innerHTML=`<img src="https://xploit.men/scdn/?fluenticons&name=chevron-right" alt=">" loading="lazy">`})
-        Array.from(document.querySelectorAll('.cm-selectionMatch')).forEach(elem => {spaces(elem.innerText) ? elem.classList.add("cm-spacematch") : elem.classList.remove("cm-spacematch")})
-        
-        document.querySelectorAll("span").forEach(span => {
-            if(span.innerText===">") {
-                var spanstyle = window.getComputedStyle(span);
-                var cssvarjson = hexToRgb(spanstyle.getPropertyValue('--cm-md-meta'))
-                var cssvar = `rgb(${cssvarjson.r}, ${cssvarjson.g}, ${cssvarjson.b})`
-                if(cssvar === spanstyle.color) {
-                    span.classList.add("cm-md-quote")
-                }
-            }
-        })
-
+        loadDetails();
     }, 1)
 
     // Format: https://airaurl/editor-nocookie/lang/base64editorcontent
---
+++ src/utils/codemirror/details.js
@@ -0,0 +1,19 @@
+export function loadDetails() {
+            // Details
+            Array.from(document.getElementsByClassName("cm-selectionBackground")).forEach(elem => { elem.style.setProperty('background', '#243047', 'important'); }) 
+            Array.from(document.querySelectorAll('span[title="Fold line"]')).forEach(elem => {elem.innerHTML=`<img src="https://xploit.men/scdn/?fluenticons&name=chevron-down" alt="v" loading="lazy">`})
+            Array.from(document.querySelectorAll('span[title="Unfold line"]')).forEach(elem => {elem.innerHTML=`<img src="https://xploit.men/scdn/?fluenticons&name=chevron-right" alt=">" loading="lazy">`})
+            Array.from(document.querySelectorAll('.cm-selectionMatch')).forEach(elem => {spaces(elem.innerText) ? elem.classList.add("cm-spacematch") : elem.classList.remove("cm-spacematch")})
+            
+            document.querySelectorAll("span").forEach(span => {
+                if(span.innerText===">") {
+                    var spanstyle = window.getComputedStyle(span);
+                    var cssvarjson = hexToRgb(spanstyle.getPropertyValue('--cm-md-meta'))
+                    var cssvar = `rgb(${cssvarjson.r}, ${cssvarjson.g}, ${cssvarjson.b})`
+                    if(cssvar === spanstyle.color) {
+                        span.classList.add("cm-md-quote")
+                    }
+                }
+            })
+    
+}
