9b566f3f2209bf100d43d2082a3a0031e2de5b70
pico190 <saulbuesosanz@gmail.com>
2024-03-24 13:53:28 +0100

    svgupdate

--- src/utils/codemirror/details.js
+++ src/utils/codemirror/details.js
@@ -18,13 +18,4 @@ export function loadDetails() {
                     }
                 }
             })
-    
-            var svg = document.getElementById("svgroundcorner").innerHTML = `
-                            <defs>
-                                <filter id="selection-goo">
-                                <feGaussianBlur in="SourceGraphic" stdDeviation="1" result="blur"></feGaussianBlur>
-                                <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 19 -9" result="goo"></feColorMatrix>
-                                <feComposite in="SourceGraphic" in2="goo" operator="atop"></feComposite>
-                                </filter>
-                            </defs>`;
 }
