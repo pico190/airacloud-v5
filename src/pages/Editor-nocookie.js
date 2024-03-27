6edb51019c0cf4fa6fe8a8e9318a23deab879e5c
pico190 <saulbuesosanz@gmail.com>
2024-03-27 19:33:58 +0100

    -extension crash

--- src/pages/Editor-nocookie.js
+++ src/pages/Editor-nocookie.js
@@ -13,7 +13,6 @@ import $ from 'jquery'
 import { spaces, hexToRgb } from "../utils/generalfuncs";
 import langLoader from "../utils/codemirror/langloader";
 import { loadDetails } from "../utils/codemirror/details";
-import { spaceMarkExtension } from "../utils/codemirror/built-in extensions/codemirror-spaces";
 
 import { vscodeKeymap } from "@replit/codemirror-vscode-keymap";
 import { keymap } from '@codemirror/view';
@@ -84,7 +83,6 @@ export function EditorNoCookie({ urlparsed }) {
                             activeDark: '#FFFFFF4F',
                         }
                     }),
-                    spaceMarkExtension(),
                     rainbowBrackets(),
                     inlineSuggestion({ fetchFn: fetchSuggestion, delay: 1000, })
                 ]
@@ -96,7 +94,6 @@ export function EditorNoCookie({ urlparsed }) {
                     hyperLink, 
                     color, 
                     keymap.of(vscodeKeymap),
-                    spaceMarkExtension(),
                     indentationMarkers(),
                     rainbowBrackets(),
                     inlineSuggestion({ fetchFn: fetchSuggestion, delay: 1000, })
