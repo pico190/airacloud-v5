0c42efa649caa982078c31ea96214e8038a127ca
pico190 <saulbuesosanz@gmail.com>
2024-03-28 20:33:38 +0100

    nova

--- src/utils/codemirror/lints/lintmsg.js
+++ src/utils/codemirror/lints/lintmsg.js
@@ -1,11 +1,15 @@
 
 export function lintmsg(errors) {
-    Array.from(document.querySelector(".cm-content").children).forEach((line, index) => {
-        errors.forEach(error => {
-            if(error.line == index+1) {
-                line.style.backgroundColor = "red"
-                console.log(line.innerText)
-            }
+    try {
+        Array.from(document.querySelector(".cm-content").children).forEach((line, index) => {
+            errors.forEach(error => {
+                if(error.line == index+1) {
+                    line.style.backgroundColor = "red"
+                    console.log(line.innerText)
+                }
+            })
         })
-    })
+    } catch(err) {
+        return false;
+    }
 }
