c4ebc3a1f2505c9098e8b204495ae7a205ce7cc3
pico190 <saulbuesosanz@gmail.com>
2024-03-28 20:42:35 +0100

    nose

--- src/utils/codemirror/lints/lintmsg.js
+++ src/utils/codemirror/lints/lintmsg.js
@@ -1,10 +1,24 @@
+import { jsxtostr } from "../../generalfuncs"
 
 export function lintmsg(errors) {
     try {
         Array.from(document.querySelector(".cm-content").children).forEach((line, index) => {
             errors.forEach(error => {
                 if(error.line == index+1) {
-                    line.style.backgroundColor = "var(--cm-errorLineBg)"
+                    var editorcontainer = document.querySelector(".editorcontainer")
+                    
+                    editorcontainer.innerHTML = editorcontainer.innerHTML + jsxtostr(
+                        <div 
+                            style={{ 
+                                position: "absolute", 
+                                backgroundColor: "var(--cm-errorLineBg)",
+                                width: "100%",
+                                height: line.offsetHeight + "px",
+                                left: "0px",
+                                top: line.offsetTop + "px"
+                            }} 
+                        />
+                    )
                     console.log(line.innerText)
                 }
             })
