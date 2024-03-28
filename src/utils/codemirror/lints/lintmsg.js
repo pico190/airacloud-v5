ecfe2a437a652186a30d3fd9a1a300a74490e7fe
pico190 <saulbuesosanz@gmail.com>
2024-03-28 22:40:47 +0100

    vxzds

--- src/utils/codemirror/lints/lintmsg.js
+++ src/utils/codemirror/lints/lintmsg.js
@@ -4,19 +4,18 @@ export function lintmsg(errors) {
     try {
         Array.from(document.querySelector(".cm-content").children).forEach((line, index) => {
             
-            line.classList.remove("errorline")
             errors.forEach(error => {
                 if(error.line == index+1) {
                     
                     line.classList.add("errorline")
                     
+                } else {
+                    
+                    line.classList.remove("errorline")
                 }
             })
         })
         Array.from(document.querySelector(".cm-gutter").children).forEach((line, index) => {
-            
-            line.classList.remove("errorline")
-            line.classList.remove("errorlinegutter")
             
             errors.forEach(error => {
                 if(error.line == parseInt(line.innerText)) {
@@ -24,6 +23,10 @@ export function lintmsg(errors) {
                     line.classList.add("errorline")
                     line.classList.add("errorlinegutter")
                     
+                } else {
+                    
+                    line.classList.add("errorline")
+                    line.classList.add("errorlinegutter")
                 }
             })
         })
