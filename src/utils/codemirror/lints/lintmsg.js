6c430a993812567849e722de7768b532348fc819
pico190 <saulbuesosanz@gmail.com>
2024-03-29 11:31:32 +0100

    11 limit

--- src/utils/codemirror/lints/lintmsg.js
+++ src/utils/codemirror/lints/lintmsg.js
@@ -12,8 +12,41 @@ export function lintmsg(errors, setClassNameExtension) {
             })
         })
         document.getElementById("linter").innerHTML = `
+        
+        .errorline:nth-of-type(1)::before {
+            content: "${errors[0].line + ""}";
+        }
+        .errorline:nth-of-type(2)::before {
+            content: "${errors[1].line + ""}";
+        }
+        .errorline:nth-of-type(3)::before {
+            content: "${errors[2].line + ""}";
+        }
+        .errorline:nth-of-type(4)::before {
+            content: "${errors[3].line + ""}";
+        }
+        .errorline:nth-of-type(5)::before {
+            content: "${errors[4].line + ""}";
+        }
+        .errorline:nth-of-type(6)::before {
+            content: "${errors[5].line + ""}";
+        }
+        .errorline:nth-of-type(7)::before {
+            content: "${errors[6].line + ""}";
+        }
+        .errorline:nth-of-type(8)::before {
+            content: "${errors[7].line + ""}";
+        }
+        .errorline:nth-of-type(9)::before {
+            content: "${errors[8].line + ""}";
+        }
+        .errorline:nth-of-type(10)::before {
+            content: "${errors[9].line + ""}";
+        }
+        .errorline:nth-of-type(11)::before {
+            content: "${errors[10].line + ""}";
+        }
         .errorline::before {
-            content: "${errors.line}";
             height: ${document.querySelector(".cm-line").offsetHeight}px;
             width: 100%;
             background-color: var(--cm-errorLineBg);
