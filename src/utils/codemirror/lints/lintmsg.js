b6b343cb7e1317e7a523e47b8fc30bfcfb76b4b9
pico190 <saulbuesosanz@gmail.com>
2024-03-29 21:28:14 +0100

    +warnlinter

--- src/utils/codemirror/lints/lintmsg.js
+++ src/utils/codemirror/lints/lintmsg.js
@@ -9,26 +9,30 @@ export function lintmsg(errors) {
 
         errors.forEach((error, index) => {
                 console.log(error, "\\", errors)
+                var type = "error"
+                if(error.type === "warn") {
+                    type = "warn"
+                }
                 cssgenerated += `
                 .cm-line:nth-of-type(${(error.line) + ""}) {
-                    background-color: var(--cm-errorLineBg)!important;
+                    background-color: var(--cm-${type}LineBg)!important;
                 }
                 .cm-gutterElement:nth-of-type(${(error.line + 1) + ""}) {
-                    background-color: var(--cm-errorLineBg)!important;
-                    color: var(--cm-error-gutter)!important;
+                    background-color: var(--cm-${type}LineBg)!important;
+                    color: var(--cm-${type}-gutter)!important;
                 }
                 .cm-line:nth-of-type(${(error.line) + ""})::after {
                     content: "         ${error.message}"!important;
                     position: absolute;
                     top: 0px;
-                    color: var(--cm-error-gutter)!important;
+                    color: var(--cm-${type}-gutter)!important;
                 }
                 .cm-gutterElement:nth-of-type(${(error.line + 1) + ""})::after {
                     content: "";
                     position: absolute;
                     right: 0px;
                     height: ${document.querySelector(".cm-line") ? document.querySelector(".cm-line").offsetHeight + "px" : "calc(var(--editor-font-size) + 1vh)"};
-                    background-color: var(--cm-errorLineBg);
+                    background-color: var(--cm-${type}LineBg);
                     width: 30px;
                   }
                 `;
