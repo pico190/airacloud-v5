ac12f9f4e72c5cb8622c068de9fa66bebacda875
pico190 <saulbuesosanz@gmail.com>
2024-03-29 19:55:56 +0100

    asasa

--- src/utils/codemirror/lints/lintmsg.js
+++ src/utils/codemirror/lints/lintmsg.js
@@ -4,45 +4,29 @@ export function lintmsg(errors) {
     // try {
         
     
-        let cssgenerated = ``;
+        var cssgenerated = ``;
 
 
         errors.forEach((error, index) => {
                 console.log(error, "\\", errors)
                 cssgenerated += `
-                .errorline:nth-of-type(${(index + 1) + ""})::before {
-                    content: "${error.line + ""}"!important;
+                .cm-line:nth-of-type(${(index + 1) + ""})::before {
+                    background-color: var(--cm-errorLineBg);
                 }
-                .errorline:nth-of-type(${(index + 1) + ""}) {
-                    background-color: red;
+                .cm-gutterElement:nth-of-type(${(index + 2) + ""})::before {
+                    background-color: var(--cm-errorLineBg);
                 }
-                .errorline:nth-of-type(${(index + 1) + ""})::after {
-                    content: "${error.message}"!important;
+                .cm-line:nth-of-type(${(index + 1) + ""})::after {
+                    content: "          ${error.message}"!important;
                 }`;
         });
         
         console.log("CSS 1 > ", cssgenerated);
         
-        var cssresult = `${cssgenerated}
-        .errorline::before {
-            content: "0";
-            height: ${document.querySelector(".cm-line") ? document.querySelector(".cm-line").offsetHeight + "" : 0 + ""}px;
-            width: 100%;
-            background-color: var(--cm-errorLineBg);
-            position: absolute;
-            left: -100%;
-            z-index: 1000;
-            font-family: var(--editor-font-family);
-            font-size: var(--editor-font-family);
-            color: var(--cm-error-gutter);
-            text-align: right;
-            padding-right: 28px;
-            box-sizing: border-box;
-        }`;
         
         var linterElement = document.getElementById("linter");
         
-        linterElement.innerHTML = cssresult;
+        linterElement.innerHTML = cssgenerated;
         
     // } catch(err) {
     //     return false;
