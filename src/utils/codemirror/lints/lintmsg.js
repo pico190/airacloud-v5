7adcd50b424d8dc40d06281d1aee511109f0b712
pico190 <saulbuesosanz@gmail.com>
2024-03-29 11:38:28 +0100

    dssgsg

--- src/utils/codemirror/lints/lintmsg.js
+++ src/utils/codemirror/lints/lintmsg.js
@@ -15,11 +15,13 @@ export function lintmsg(errors, setClassNameExtension) {
             cssgenerated = cssgenerated + `
             .errorline:nth-of-type(${(index + 1) + ""})::before {
                 content: "${error.line + ""}"!important;
+            }
+            .errorline:nth-of-type(${(index + 1) + ""})::after {
+                content: "${error.message}"!important;
             }`
         })
         
-        document.getElementById("linter").innerHTML = `
-        ${cssgenerated}
+        var cssresult = `${cssgenerated}
         .errorline::before {
             content : "0";
             height: ${document.querySelector(".cm-line").offsetHeight}px;
@@ -35,6 +37,10 @@ export function lintmsg(errors, setClassNameExtension) {
             padding-right: 28px;
             box-sizing: border-box;
           }`
+
+        if(!document.getElementById("linter").innerHTML === cssresult) {
+            document.getElementById("linter").innerHTML = cssresult
+        }
     } catch(err) {
         return false;
     }
