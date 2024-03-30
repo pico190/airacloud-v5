8f67edd27ab8cc5a57c9e6ede07379f9c3167fcf
pico190 <saulbuesosanz@gmail.com>
2024-03-30 18:29:59 +0100

    Positionf ix

--- src/utils/codemirror/intelli/principal.js
+++ src/utils/codemirror/intelli/principal.js
@@ -4,7 +4,7 @@ export function loadIntelli() {
     var cursor = document.querySelector("#cm-cursor-primary");
 
     intellisense.innerHTML = "<h1>a</h1>";
-    intellisense.left = cursor.offsetLeft + "px";
-    intellisense.top = cursor.offsetTop + "px";
+    intellisense.style.left = cursor.offsetLeft + "px";
+    intellisense.style.top = cursor.offsetTop + "px";
 
 }
