dd8e0fae615b96f8af7022b540751abe6e798ca6
pico190 <saulbuesosanz@gmail.com>
2024-03-30 20:24:24 +0100

    asHGJSAnpokewru9tr7

--- src/utils/codemirror/intelli/principal.js
+++ src/utils/codemirror/intelli/principal.js
@@ -6,14 +6,11 @@ export function loadIntelli(intelli) {
     var cursor = document.querySelector(".cm-cursor-primary");
 
     var elementsline = document.querySelector(".cm-activeLine").querySelectorAll('[class*="ͼ"]')
-    elementsline.forEach(element => {
-        
-    })
     var editorToken = nearElem(elementsline, cursor)
     console.log(elementsline)
 
     intellisense.innerHTML = "<h1>"+editorToken.innerText+"</h1>";
-    intellisense.style.left = (cursor.offsetLeft - 3) + "px";
-    intellisense.style.top = (cursor.offsetTop - 20) + "px";
+    intellisense.style.left = (cursor.getBoundingClientRect().left - 3) + "px";
+    intellisense.style.top = (cursor.getBoundingClientRect().top - 20) + "px";
 
 }
