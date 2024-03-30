1c0887fbe16159534977ec6de38c0c57fce00027
pico190 <saulbuesosanz@gmail.com>
2024-03-30 09:38:19 +0100

    jdhjkdahjkdsahkjdsf cocacola epsuma

--- src/utils/codemirror/lints/lintmsg.js
+++ src/utils/codemirror/lints/lintmsg.js
@@ -24,44 +24,50 @@ export function lintmsg(errors) {
                     }
 
                     var gutterTop = 0;
-                    document.querySelectorAll(".cm-lineNumbers > .cm-gutterElement") ? document.querySelectorAll(".cm-lineNumbers > .cm-gutterElement").forEach((gutter, index) => {
-                        if(parseInt(gutter.innerText) === error.line) {
-                            cssgenerated += `
-                            .cm-lineNumbers > .cm-gutterElement:nth-of-type(${(index + 1) + ""}) {
-                                background-color: var(--cm-${type}LineBg)!important;
-                                color: var(--cm-${type}-gutter)!important;
-                            }
-                            .cm-lineNumbers > .cm-gutterElement:nth-of-type(${(index + 1) + ""})::after {
-                                content: "";
-                                position: absolute;
-                                right: 0px;
-                                height: ${document.querySelector(".cm-line") ? document.querySelector(".cm-line").offsetHeight + "px" : "calc(var(--editor-font-size) + 1vh)"};
-                                background-color: var(--cm-${type}LineBg);
-                                width: 30px;
-                              }
-                            `;
-                            gutterTop = gutter.offsetTop;
-                        }     
-                    }) : false
+                    var gutters = document.querySelectorAll(".cm-lineNumbers > .cm-gutterElement") ? document.querySelectorAll(".cm-lineNumbers > .cm-gutterElement") : false
+                    if(gutters) {
+                        gutters.forEach((gutter, index) => {
+                            if(parseInt(gutter.innerText) === error.line) {
+                                cssgenerated += `
+                                .cm-lineNumbers > .cm-gutterElement:nth-of-type(${(index + 1) + ""}) {
+                                    background-color: var(--cm-${type}LineBg)!important;
+                                    color: var(--cm-${type}-gutter)!important;
+                                }
+                                .cm-lineNumbers > .cm-gutterElement:nth-of-type(${(index + 1) + ""})::after {
+                                    content: "";
+                                    position: absolute;
+                                    right: 0px;
+                                    height: ${document.querySelector(".cm-line") ? document.querySelector(".cm-line").offsetHeight + "px" : "calc(var(--editor-font-size) + 1vh)"};
+                                    background-color: var(--cm-${type}LineBg);
+                                    width: 30px;
+                                  }
+                                `;
+                                gutterTop = gutter.offsetTop;
+                            }     
+                        })
+                    }
                     var lineNumbersSum = 1;
                     if(document.querySelector("cm-content") ? document.querySelector("cm-content").firstChild.classList.includes("cm-gap") : false) {
                         lineNumbersSum = 2
                     }
-                    document.querySelectorAll(".cm-lineNumbers > .cm-gutterElement") ? document.querySelectorAll(".cm-lineNumbers > .cm-gutterElement").forEach((line, index) => {
-                        if(line.offsetTop === gutterTop) {
-                            cssgenerated += `
-                            .cm-line:nth-of-type(${index + lineNumbersSum}) {
-                                background-color: var(--cm-${type}LineBg)!important;
-                            }
-                            .cm-line:nth-of-type(${index + lineNumbersSum})::after {
-                                content: "       ${error.message}"!important;
-                                position: absolute;
-                                top: 0px;
-                                color: var(--cm-${type}-gutter)!important;
-                            }
-                            `;
-                        }     
-                    }) : false
+                    var line = document.querySelectorAll(".cm-line") ? document.querySelectorAll(".cm-lineNumbers > .cm-gutterElement") : false
+                    if(line) {
+                        line.forEach((line, index) => {
+                            if(line.offsetTop === gutterTop) {
+                                cssgenerated += `
+                                .cm-line:nth-of-type(${index + lineNumbersSum}) {
+                                    background-color: var(--cm-${type}LineBg)!important;
+                                }
+                                .cm-line:nth-of-type(${index + lineNumbersSum})::after {
+                                    content: "       ${error.message}"!important;
+                                    position: absolute;
+                                    top: 0px;
+                                    color: var(--cm-${type}-gutter)!important;
+                                }
+                                `;
+                            }     
+                        })
+                    }
                 }
         });
         
