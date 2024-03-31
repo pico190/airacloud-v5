015c9136dab09be83b0a91519f5de20a4907ca00
pico190 <saulbuesosanz@gmail.com>
2024-03-31 22:43:46 +0200

    v5.4.4 [+svg, +yaml, +xml, +properties]

--- src/utils/codemirror/langloader.js
+++ src/utils/codemirror/langloader.js
@@ -2,6 +2,7 @@ import { decode } from 'js-base64';
 import { pythonLanguage } from "@codemirror/lang-python";
 import { jsonLanguage } from "@codemirror/lang-json";
 import { html, CompletionContext } from "@codemirror/lang-html";
+import { properties } from "@codemirror/legacy-modes/mode/properties.js";
 
 export default function langLoader(lang, langs) {
 
@@ -50,7 +51,11 @@ export default function langLoader(lang, langs) {
     else if (lang === "rust") { lng = langs.rust() }
     else if (lang === "lua") { lng = langs.lua() }
     else if (lang === "cpp") { lng = langs.cpp() }
+    else if (lang === "svg") { lng = langs.svg() }
+    else if (lang === "yaml") { lng = langs.yaml() }
+    else if (lang === "xml") { lng = langs.xml(); document.getElementById("langstyle").innerHTML = "* {--cm-tag-name: var(--cm-tag-name-standard)!important;}" }
     else if (lang === "angular") { lng = langs.angular(); document.getElementById("langstyle").innerHTML = "* {--cm-tag-name: var(--cm-tag-name-standard)!important;}" }
+    else if (lang === "properties") { lng = properties; }
     else { lng = null }
     return lng;
 }
