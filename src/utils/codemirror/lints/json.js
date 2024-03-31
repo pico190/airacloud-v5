45fe39d1c58fc9998c25389d16aa22a59f1e6eb2
pico190 <saulbuesosanz@gmail.com>
2024-03-31 23:21:45 +0200

    +jsonlinter

--- src/utils/codemirror/lints/json.js
+++ src/utils/codemirror/lints/json.js
@@ -5,11 +5,23 @@
  * @param {function} setErrors - Function to set syntax errors
  */
 export default function jsonLinter(fullcode, setErrors) {
-    console.log("funcoam")
+    var result = []
     try {
         JSON.parse(fullcode)
     } catch(err) {
-        console.log(err);
+        var line = err.split("line")[1].split("column")[0].trim()
+        var index = err.split("column")[1].split("of")[0].trim()
+
+        result.push({
+            line: line,
+            index: index,
+            code: fullcode.split("\n")[line-1],
+            message: err,
+            type: 'error',
+            codelines: fullcode.split("\n")
+        })
     }
+
+    setErrors(result);
 
 }
