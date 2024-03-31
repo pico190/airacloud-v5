1f563a0fb47e07bbd9022f02d7168742d4c248bd
pico190 <saulbuesosanz@gmail.com>
2024-03-31 23:10:54 +0200

    funciona

--- src/utils/codemirror/lints/json.js
+++ src/utils/codemirror/lints/json.js
@@ -5,7 +5,7 @@
  * @param {function} setErrors - Function to set syntax errors
  */
 export default function jsonLinter(fullcode, setErrors) {
-
+    console.log("funcoam")
     try {
         JSON.parse(fullcode)
     } catch(err) {
