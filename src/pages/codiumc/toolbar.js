8ec7f8e46785d28e2936ca06112cc6b66338fc9e
pico190 <saulbuesosanz@gmail.com>
2024-04-02 18:34:37 +0200

    asasasasasasas

--- src/pages/codiumc/toolbar.js
+++ src/pages/codiumc/toolbar.js
@@ -1,4 +1,7 @@
-import { Ic_Outline_DocumentMultiple, Ic_Filled_DocumentMultiple } from "../../utils/fluenticons";
+import { 
+    Ic_Outline_DocumentMultiple, Ic_Filled_DocumentMultiple,
+    Ic_Outline_Settings, Ic_Filled_Settings
+} from "../../utils/fluenticons";
 
 export default function Toolbar() {
 
@@ -9,23 +12,41 @@ export default function Toolbar() {
         if(!elem.classList.contains("codium-toolbaritem-active")) {
             var name = elem.getAttribute("name");
             window.location.href = "/codium/#" + name;
+            var activelements = document.querySelectorAll(".codium-toolbaritem-active");
+            activelements.forEach(activeelem => {activeelem.classList.remove("codium-toolbaritem-active")})
+
             elem.classList.add("codium-toolbaritem-active");
         }
     }
 
+    var defaultName = "files";
+
+    window.addEventListener("load", () => {
+        var defaultelem = document.querySelector('div.codium-toolbaritem[name="'+defaultName+'"]');
+        window.location.href = "/codium/#" + defaultName;
+    })
+
     return (
         <div className="codium-toolbar">
+
+            {/* Files, Default Item */}
             <div className="codium-toolbaritem" onClick={() => {click("files")}} name="files">
                 <Ic_Outline_DocumentMultiple id="files" />
                 <div className="codium-toolbaritem-svg-active" style={{display: "none"}}>
                     <Ic_Filled_DocumentMultiple id="files-active" />
                 </div>
             </div>
+
+
+
+
             <div className="codium-spacer"></div>
+            
+            {/* Settings Item */}
             <div className="codium-toolbaritem" onClick={() => {click("settings")}} name="settings">
-                <Ic_Outline_DocumentMultiple id="files" />
+                <Ic_Outline_Settings id="files" />
                 <div className="codium-toolbaritem-svg-active" style={{display: "none"}}>
-                    <Ic_Filled_DocumentMultiple id="files-active" />
+                    <Ic_Filled_Settings id="files-active" />
                 </div>
             </div>
         </div>
